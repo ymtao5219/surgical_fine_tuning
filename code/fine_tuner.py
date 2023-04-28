50a212eea89cf9ecf8ab9cb7bdbfa892d8bfa099
ymtao5219 <ytao5219@gmail.com>
2023-04-28 05:07:22 +0000

    fix bugs when passing list of layers to freeze

--- code/fine_tuner.py
+++ code/fine_tuner.py
@@ -30,11 +30,12 @@ def main(args):
     def add_prefix(val):
         return "bert.encoder.layer." + str(val)
 
+    # print("layers to freeze", freeze_layers)
     if freeze_layers:
         for i in range(len(freeze_layers)):
             freeze_layers[i] = add_prefix(freeze_layers[i])
         freeze_layers = tuple(freeze_layers)
-
+        print("layers to freeze", freeze_layers)
         for name, param in model.named_parameters():
             if name.startswith(freeze_layers):
                 param.requires_grad = False
@@ -84,7 +85,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fine-tuning a parent model")
     parser.add_argument("--parent_model", type=str, default="bert-base-cased", help="Name of the parent model to use from Hugging Face")
     parser.add_argument("--task_name", type=str, default="wic", help="Name of the task in GLUE/SuperGLUE to fine-tune on")
-    parser.add_argument("--freeze_layers", type=list, default=[], help="List of which layers to freeze")
+    parser.add_argument("--freeze_layers", nargs='+', type=int, help="List of which layers to freeze")
     args = parser.parse_args()
 
     main(args)
