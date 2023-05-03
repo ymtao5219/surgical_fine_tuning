9e4cd2d23cab9e44b38e475910e460c2330e084f
ymtao5219 <ytao5219@gmail.com>
2023-05-03 08:14:25 +0000

    add helper function to fix random seeds

--- code/utils.py
+++ code/utils.py
@@ -3,6 +3,15 @@ import json
 import matplotlib.pyplot as plt
 import random
 import pickle 
+import torch
+
+def set_random_seed(seed: int):
+    """set seeds for reproducibility"""
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
 
 def load_dict_from_pickle(file_path):
     with open(file_path, 'rb') as f:
