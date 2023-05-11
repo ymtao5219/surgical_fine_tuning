5236cf04e45848d8633db839d9bde305bcfea24a
gbelapurkar_umass_edu <gayatri.belapurkar5@gmail.com>
2023-05-11 14:46:33 +0000

    Logs for runs

--- scripts/GLUE/qnli.sh
+++ scripts/GLUE/qnli.sh
@@ -7,20 +7,20 @@
 #SBATCH -G 1  # Number of GPUs
 
 
-echo "######################################################################"
-echo "full model fine-tuning"
-python code/fine_tuner.py --task_name qnli
+# echo "######################################################################"
+# echo "full model fine-tuning"
+# python code/fine_tuner.py --task_name qnli
 
-echo "######################################################################"
-# top layers based on fisher information: [1, 3, 4, 5, 0]
-echo "layer-wise fine-tuning top 1"
-python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 3 4 5 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 2"
-python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 4 5 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 3"
-python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 5 6 7 8 9 10 11
+# echo "######################################################################"
+# # top layers based on fisher information: [1, 3, 4, 5, 0]
+# echo "layer-wise fine-tuning top 1"
+# python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 3 4 5 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 2"
+# python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 4 5 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 3"
+# python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 5 6 7 8 9 10 11
 echo "######################################################################"
 echo "layer-wise fine-tuning top 4"
 python code/fine_tuner.py --task_name qnli --freeze_layers 0 2 6 7 8 9 10 11
