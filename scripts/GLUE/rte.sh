5683e74c6fba70f64637fb81f2b644228d1c389b
gbelapurkar_umass_edu <gayatri.belapurkar5@gmail.com>
2023-05-18 14:03:54 +0000

    Logs and changed hyperparameters

--- scripts/GLUE/rte.sh
+++ scripts/GLUE/rte.sh
@@ -2,31 +2,34 @@
 #SBTACH --job-name=glue_rte  # CHANGE THIS! for different tasks
 #SBATCH --output=results/logs/%x_job_name_output_%j.log
 #SBATCH -c 4  # Number of Cores per Task
-#SBATCH --mem=50G  # Requested Memory
 #SBATCH -p gypsum-titanx  # Partition
 #SBATCH -G 1  # Number of GPUs
 
+echo "######################################################################"
+# bottom layer 8
+echo "layer-wise fine-tuning bottom 1"
+python code/fine_tuner.py --task_name rte --freeze_layers 0 1 2 3 4 5 6 7 9 10 11
 
 # echo "######################################################################"
 # echo "full model fine-tuning"
 # python code/fine_tuner.py --task_name rte
 
-echo "######################################################################"
-# top layers based on fisher information: [1, 4, 3, 5, 0]
-echo "layer-wise fine-tuning top 1"
-python code/fine_tuner.py --task_name rte --freeze_layers 0 2 3 4 5 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 2"
-python code/fine_tuner.py --task_name rte --freeze_layers 0 2 3 5 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 3"
-python code/fine_tuner.py --task_name rte --freeze_layers 0 2 5 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 4"
-python code/fine_tuner.py --task_name rte --freeze_layers 0 2 6 7 8 9 10 11
-echo "######################################################################"
-echo "layer-wise fine-tuning top 5"
-python code/fine_tuner.py --task_name rte --freeze_layers 2 6 7 8 9 10 11
+# echo "######################################################################"
+# # top layers based on fisher information: [1, 4, 3, 5, 0]
+# echo "layer-wise fine-tuning top 1"
+# python code/fine_tuner.py --task_name rte --freeze_layers 0 2 3 4 5 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 2"
+# python code/fine_tuner.py --task_name rte --freeze_layers 0 2 3 5 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 3"
+# python code/fine_tuner.py --task_name rte --freeze_layers 0 2 5 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 4"
+# python code/fine_tuner.py --task_name rte --freeze_layers 0 2 6 7 8 9 10 11
+# echo "######################################################################"
+# echo "layer-wise fine-tuning top 5"
+# python code/fine_tuner.py --task_name rte --freeze_layers 2 6 7 8 9 10 11
 
-echo "######################################################################"
-echo "finished"
+# echo "######################################################################"
+# echo "finished"
