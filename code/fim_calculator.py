1e349151d7c6f93d2e385cc0388b4bcd2b4075fd
salonichalkapurkar <saloni.chalkapurkar@gmail.com>
2023-06-01 20:45:57 +0000

    stsb fim debugged

--- code/fim_calculator.py
+++ code/fim_calculator.py
@@ -99,6 +99,7 @@ class FIMCalculator:
             else:
                 outdx = Categorical(logits=logits).sample().unsqueeze(1).detach()
             # ipdb.set_trace()
+            outdx = outdx.to(torch.int64) # added to fix stsb error
             samples = logits.gather(1, outdx)
 
             idx, batch_size = 0, data.size(0)
@@ -181,7 +182,7 @@ SUPERGLUE_TASKS = ["cb", "multirc", "wic", "wsc", "record", "copa"]
 model_name = "bert-base-cased"
 # model_name = "bert-large-cased"
 # model_name="roberta-base"
-tokenized_data = GlueDataloader("mrpc").get_samples(100)
+tokenized_data = GlueDataloader("stsb").get_samples(100)
 
 # ipdb.set_trace()
 calc = FIMCalculator(model_name, tokenized_data)
