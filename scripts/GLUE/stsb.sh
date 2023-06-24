0dcf36dddd0df2b40eeff30bc1fab8a62b7d973d
salonichalkapurkar <saloni.chalkapurkar@gmail.com>
2023-06-24 01:11:07 +0000

    merge

--- scripts/GLUE/stsb.sh
+++ scripts/GLUE/stsb.sh
@@ -13,26 +13,32 @@ python code/fine_tuner.py --task_name stsb
 
 
 # echo "######################################################################"
-# echo "full model fine-tuning"
-# python code/fine_tuner.py --task_name stsb
-
-
-echo "######################################################################"
-# top layers based on fisher information: [5, 1, 4, 3, 2]
-echo "layer-wise fine-tuning top 1"
-python code/fine_tuner.py --task_name stsb --freeze_layers 0 1 2 3 4 6 7 8 9 10 11 
+# # top layers based on fisher information: [ 2,1, 6, 4, 3] 2,1, 6, 4, 3, 5, 0, 7, 11, 8, 10, 9
+# echo "layer-wise fine-tuning top 1"
+# python code/fine_tuner.py --task_name stsb --freeze_layers 0 1 3 4 5 6 7 8 9 10 11 
 # echo "######################################################################"
 # echo "layer-wise fine-tuning top 2"
-# python code/fine_tuner.py --task_name stsb --freeze_layers 0 2 3 4 6 7 8 9 10 11 
+# python code/fine_tuner.py --task_name stsb --freeze_layers 0 3 4 5 6 7 8 9 10 11 
 # echo "######################################################################"
 # echo "layer-wise fine-tuning top 3"
-# python code/fine_tuner.py --task_name stsb --freeze_layers 0 2 3 6 7 8 9 10 11 
+# python code/fine_tuner.py --task_name stsb --freeze_layers 0 3 4 5 7 8 9 10 11 
 # echo "######################################################################"
 # echo "layer-wise fine-tuning top 4"
-# python code/fine_tuner.py --task_name stsb --freeze_layers 0 2 6 7 8 9 10 11 
+# python code/fine_tuner.py --task_name stsb --freeze_layers 0 3 5 7 8 9 10 11
 # echo "######################################################################"
 # echo "layer-wise fine-tuning top 5"
-# python code/fine_tuner.py --task_name stsb --freeze_layers 0 6 7 8 9 10 11 
+# python code/fine_tuner.py --task_name stsb --freeze_layers 0 5 7 8 9 10 11
+
+# echo "######################################################################"
+# echo "finished"
+
+echo "######################################################################"
+echo "layer-wise fine-tuning bottom 1" #2,1, 6, 4, 3, 5, 0, 7, 11, 8, 10, 9
+python code/fine_tuner.py --task_name stsb --freeze_layers 0 1 2 3 4 5 6 7 8 10 11
+
+echo "######################################################################"
+echo "layer-wise fine-tuning bottom 2"
+python code/fine_tuner.py --task_name stsb --freeze_layers 0 1 2 3 4 5 6 7 8 11
 
 echo "######################################################################"
 echo "layer-wise fine-tuning bottom 5"
